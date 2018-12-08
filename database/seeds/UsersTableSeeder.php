80c33e5ccf7a0057e214f1a8db6314858c9ba630
OLDBIG9 <1901914035@qq.com>
2018-12-08 16:08:49 +0800

    init commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'=>'OLDBIG9',
+            'email'=>'oldbig9@qq.com',
+            'password'=>'$2y$10$geLEyijJ149BSB2SOPdYiep5tpUDglZGDIQzNk8gcCT0x1eYkRy9K'
+        ]);
+    }
+}
